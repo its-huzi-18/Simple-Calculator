892e0390be1d8fc95733562a01bb65d7cc3c18de
Daniee <hu98872676@gmail.com>
2024-03-22 00:52:53 +0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # Simple-Calculator
 A simple calculator using typeScript
+=======
+"# simple-calculator" 
+>>>>>>> 14feaea (push my calculator to github)
---
+++ app.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        message: "Enter First Number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter Second Number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subraction", "Multiplication", "Division", "Remender"],
+    }
+]);
+//   conditions
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operators === "Remender") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else {
+    console.log("Please select the valid operator");
+}
